49d19dcd117ff23b76408a3813d63499d074b755
Lee Seaver <lee.seaver@gmail.com>
2023-04-03 22:49:38 +0000

    Client login

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,39 +2,19 @@ import { useEffect, useState } from 'react';
 import DummyList from './Construct.js'
 import ErrorNotification from './ErrorNotification';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ClientSignUpForm from './ClientSignUp.js';
+
 
 function App() {
-  // const [launch_info, setLaunchInfo] = useState([]);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   async function getData() {
-  //     let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-  //     console.log('fastapi url: ', url);
-  //     let response = await fetch(url);
-  //     console.log("------- hello? -------");
-  //     let data = await response.json();
-
-  //     if (response.ok) {
-  //       console.log("got launch data!");
-  //       setLaunchInfo(data.launch_details);
-  //     } else {
-  //       console.log("drat! something happened");
-  //       setError(data.message);
-  //     }
-  //   }
-  //   getData();
-  // }, [])
-  // error={error}
 
 
   return (
-    <>
-      <div>
-        <ErrorNotification />
-        <DummyList />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="clientsignup" element={<ClientSignUpForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
