5c85a66f4e75ced36868eb9dac4e14f603794069
kenneth wilson <kennethwilson0619@gmail.com>
2023-04-07 15:47:50 -0400

    Create Service; Get All Services work via business id.

--- cruisecontrol/routers/services.py
+++ cruisecontrol/routers/services.py
@@ -6,6 +6,16 @@ from authenticator import authenticator
 
 router = APIRouter()
 
+
+@router.get("/api/services", response_model=List[ServiceOut])
+def get_all_services(
+    repo: ServiceQueries = Depends(),
+    account_data = Depends(authenticator.get_current_account_data)
+):
+    business_id = account_data['business_id']
+    print('************', business_id)
+    return repo.get_all(repo, business_id)
+
 @router.post("/api/services", response_model=ServiceOut)
 def create_service(
     info: ServiceIn,
