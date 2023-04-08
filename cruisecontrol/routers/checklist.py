ac31e108c3ebc4d43d481ac393b52b273878cb33
Joshua <jpe7619@gmail.com>
2023-04-07 17:33:17 -0700

    completed and tested end points for checklist

--- cruisecontrol/routers/checklist.py
+++ cruisecontrol/routers/checklist.py
@@ -39,10 +39,12 @@ router = APIRouter()
 @router.get("/api/checklist", response_model=List[ChecklistOut])
 def get_checklist(
   repo: ChecklistQueries = Depends(),
-    account_data: dict = Depends(authenticator.get_current_account_data),
-) -> ChecklistOut:
-    business_id = account_data["business_id"]
+  account_data = Depends(authenticator.get_current_account_data),
+):
+    print(account_data["business_id"])
+    business_id = account_data['business_id']
     try:
+        print(repo)
         return repo.get_all(business_id)
     except ValueError:
         raise HTTPException(
@@ -58,10 +60,11 @@ def create_checklist(
   account_data = Depends(authenticator.get_current_account_data),
 ):
   business_id = account_data["business_id"]
-  print("****", business_id)
-
   try:
         return repo.create_checklist(info, business_id)
   except DuplicateChecklistError:
         response.status_code = status.HTTP_409_CONFLICT
         return HttpError(detail="Checklist with the same id already exists")
+  except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return HttpError(detail=str(e))
