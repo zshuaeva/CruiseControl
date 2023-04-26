d715c634f1a9223a31d3cf9d646100247431e203
Nicholas Trevino <niketrevino@live.com>
2023-04-26 19:59:04 +0000

    Cicd

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -21,9 +21,6 @@ import AppointmentEdit from "./AppointmentUpdate.js";
 function App() {
   const domain = /https:\/\/[^/]+/;
   const basename = process.env.PUBLIC_URL.replace(domain, "");
-  console.log("************", basename);
-  console.log("_____________", domain);
-  console.log("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
   return (
     <BrowserRouter basename={basename}>
       <AuthProvider
