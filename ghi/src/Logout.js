774cfa13fab05add00d1cd968847a4605172da46
Nicholas Trevino <niketrevino@live.com>
2023-04-27 01:06:17 +0000

    Cicd

--- ghi/src/Logout.js
+++ ghi/src/Logout.js
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "@galvanize-inc/jwtdown-for-react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "@galvanize-inc/jwtdown-for-react";
 
 function Logout() {
   const { setToken } = useContext(AuthContext);
@@ -9,6 +12,8 @@ function Logout() {
   const handleLogout = async () => {
     try {
       await fetch(`${process.env.REACT_APP_USER_SERVICE_API_HOST}/token`, {
+        method: "DELETE",
+        credentials: "include",
         method: "DELETE",
         credentials: "include",
       });
