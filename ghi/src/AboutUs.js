81c01b3226b494a220b4d8ca30e3e5bc8b29485d
Joshua <jpe7619@gmail.com>
2023-04-28 14:08:58 -0700

    update photos in about us, recolor logo

--- ghi/src/AboutUs.js
+++ ghi/src/AboutUs.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import lee from './team/lee.png'
+import josh from './team/josh.png'
+import ken from './team/ken.png'
+import nick from './team/nick.png'
 
 function AboutUs() {
   return (
@@ -8,16 +12,16 @@ function AboutUs() {
       <Row xs={1} md={2} lg={4} className="g-4">
         <Col>
           <Card>
-            <Card.Img variant="top" src="https://via.placeholder.com/300" />
+            <Card.Img variant="top" src={ josh } />
             <Card.Body>
               <Card.Title>Joshua Evangelista</Card.Title>
               <Card.Text>
                 "My tea's gone cold..."
               </Card.Text>
-              <Button variant="primary" href="https://gitlab.com">
+              <Button variant="primary" href="https://gitlab.com/zshuaeva">
                 GitLab
               </Button>{" "}
-              <Button variant="primary" href="https://linkedin.com">
+              <Button variant="primary" href="https://www.linkedin.com/in/joshuaevangelista/">
                 LinkedIn
               </Button>
             </Card.Body>
@@ -25,17 +29,17 @@ function AboutUs() {
         </Col>
         <Col>
           <Card>
-            <Card.Img variant="top" src="https://via.placeholder.com/300" />
+            <Card.Img variant="top" src={ lee } />
             <Card.Body>
               <Card.Title>Lee Seaver</Card.Title>
               <Card.Text>
                 "I'm wondering why
 I got out of bed at all"
               </Card.Text>
-              <Button variant="primary" href="https://gitlab.com">
+              <Button variant="primary" href="https://gitlab.com/lee.seaver">
                 GitLab
               </Button>{" "}
-              <Button variant="primary" href="https://linkedin.com">
+              <Button variant="primary" href="https://www.linkedin.com/in/lee-aaron-s">
                 LinkedIn
               </Button>
             </Card.Body>
@@ -43,22 +47,18 @@ I got out of bed at all"
         </Col>
         <Col>
           <Card>
-            <Card.Img variant="top" src="https://via.placeholder.com/300" />
+            <Card.Img variant="top" src={ nick } />
             <Card.Body>
               <Card.Title>Nicholas Trevino</Card.Title>
               <Card.Text>
                 <p>"The morning rain clouds up my window
-And I can't see at all"
-.
-.
-
-
-</p>
+                    And I can't see at all"
+                </p>
               </Card.Text>
-              <Button variant="primary" href="https://gitlab.com">
+              <Button variant="primary" href="https://gitlab.com/Ntrevino12">
                 GitLab
               </Button>{" "}
-              <Button variant="primary" href="https://linkedin.com">
+              <Button variant="primary" href="https://www.linkedin.com/in/nicholas-s-trevino/">
                 LinkedIn
               </Button>
             </Card.Body>
@@ -66,18 +66,18 @@ And I can't see at all"
         </Col>
         <Col>
           <Card>
-            <Card.Img variant="top" src="https://via.placeholder.com/300" />
+            <Card.Img variant="top" src={ ken } />
             <Card.Body>
               <Card.Title>Ken Wilson</Card.Title>
               <Card.Text>
                 "even if I could, it'd all be gray
-But your picture on my wall
-It reminds me that it's not so bad"
+                But your picture on my wall
+                It reminds me that it's not so bad"
               </Card.Text>
-              <Button variant="primary" href="https://gitlab.com">
+              <Button variant="primary" href="https://gitlab.com/kennethwilson0619">
                 GitLab
               </Button>{" "}
-              <Button variant="primary" href="https://linkedin.com">
+              <Button variant="primary" href="www.linkedin.com/in/kennethwilson0619">
                 LinkedIn
               </Button>
             </Card.Body>
